3705564f4d95699e6b8fde499d9f254963acebf7
Sertac10 <102825056+Sertac10@users.noreply.github.com>
2022-05-07 00:39:58 +0300

    Add files via upload

---
+++ SansOyunu/SansOyunu/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace SansOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnOyna = new System.Windows.Forms.Button();
+            this.lbxSayilar = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox1.Controls.Add(this.lbl6);
+            this.groupBox1.Controls.Add(this.lbl5);
+            this.groupBox1.Controls.Add(this.lbl4);
+            this.groupBox1.Controls.Add(this.lbl3);
+            this.groupBox1.Controls.Add(this.lbl2);
+            this.groupBox1.Controls.Add(this.lbl1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Location = new System.Drawing.Point(201, 114);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(516, 130);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sans Oyunu";
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl6.Location = new System.Drawing.Point(431, 45);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(66, 46);
+            this.lbl6.TabIndex = 5;
+            this.lbl6.Text = "00";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl5.Location = new System.Drawing.Point(349, 45);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(66, 46);
+            this.lbl5.TabIndex = 4;
+            this.lbl5.Text = "00";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl4.Location = new System.Drawing.Point(267, 45);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(66, 46);
+            this.lbl4.TabIndex = 3;
+            this.lbl4.Text = "00";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl3.Location = new System.Drawing.Point(185, 45);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(66, 46);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "00";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl2.Location = new System.Drawing.Point(103, 45);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(66, 46);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "00";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl1.Location = new System.Drawing.Point(21, 45);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(66, 46);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "00";
+            // 
+            // btnOyna
+            // 
+            this.btnOyna.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnOyna.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOyna.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnOyna.Location = new System.Drawing.Point(10, 11);
+            this.btnOyna.Name = "btnOyna";
+            this.btnOyna.Size = new System.Drawing.Size(120, 42);
+            this.btnOyna.TabIndex = 4;
+            this.btnOyna.Text = "OYNA";
+            this.btnOyna.UseVisualStyleBackColor = false;
+            this.btnOyna.Click += new System.EventHandler(this.btnOyna_Click);
+            // 
+            // lbxSayilar
+            // 
+            this.lbxSayilar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbxSayilar.FormattingEnabled = true;
+            this.lbxSayilar.ItemHeight = 31;
+            this.lbxSayilar.Location = new System.Drawing.Point(10, 84);
+            this.lbxSayilar.Name = "lbxSayilar";
+            this.lbxSayilar.Size = new System.Drawing.Size(120, 190);
+            this.lbxSayilar.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.ClientSize = new System.Drawing.Size(752, 347);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnOyna);
+            this.Controls.Add(this.lbxSayilar);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnOyna;
+        private System.Windows.Forms.ListBox lbxSayilar;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SansOyunu/SansOyunu/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SansOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd;
+
+        int[] sansliSayilar;
+        Label[] labels;
+
+        int sayac = 1, frekans = 10, sira = 0;
+
+        private void btnOyna_Click(object sender, EventArgs e)
+        {
+            Sifirla();
+            sayiUret();
+            timer1.Start();
+        }
+
+        private void Sifirla()
+        {
+            sira = 0;
+            lbxSayilar.Items.Clear();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (sayac++ % frekans == 0)
+            {
+                if (sira < labels.Length)
+                {
+                    lbxSayilar.Items.Add(sansliSayilar[sira].ToString().PadLeft(2, '0'));
+                    labels[sira].Text = sansliSayilar[sira].ToString().PadLeft(2, '0');
+                }
+                sira++;
+            }
+
+            //yalandan dönen sayılar..
+            for (int i = sira; i < labels.Length; i++)
+            {
+                labels[i].Text = rnd.Next(1, 50).ToString().PadLeft(2, '0');
+            }
+            System.Threading.Thread.Sleep(10);
+            if (sira > labels.Length)
+            {
+                for (int i = 0; i < labels.Length; i++)
+                {
+                    labels[i].Visible = !labels[i].Visible;
+                }
+            }
+
+            if (sira == labels.Length + 3)
+            {
+                timer1.Stop();
+                sansliSirala();
+                for (int i = 0; i < labels.Length; i++)
+                {
+                    labels[i].Visible = true;
+                    labels[i].Text = sansliSayilar[i].ToString().PadLeft(2, '0');
+                }
+            }
+        }
+
+        private void sansliSirala()
+        {
+            int temp;
+
+            for (int i = 0; i < sansliSayilar.Length - 1; i++)
+            {
+                for (int j = i + 1; j < sansliSayilar.Length; j++)
+                {
+                    if (sansliSayilar[i] > sansliSayilar[j])
+                    {
+                        temp = sansliSayilar[i];
+                        sansliSayilar[i] = sansliSayilar[j];
+                        sansliSayilar[j] = temp;
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labels = new Label[] { lbl1, lbl2, lbl3, lbl4, lbl5, lbl6 };
+        }
+
+        private void sayiUret()
+        {
+            sansliSayilar = new int[6];
+            int sansli;
+            rnd = new Random();
+            for (int i = 0; i < sansliSayilar.Length; i++)
+            {
+                do
+                {
+                    sansli = rnd.Next(1, 50);
+                } while (sansliSayilar.Contains(sansli));
+                sansliSayilar[i] = sansli;
+            }
+        }
+    
+    }
+}
